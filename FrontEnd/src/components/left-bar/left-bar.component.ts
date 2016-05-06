2c8166537491449ca2bd30e19f1efaf95c7815f7
NablaT <remi.pourtier@gmail.com>
2016-05-06 14:52:12 +0200

    Starting design of the main page

--- FrontEnd/src/components/left-bar/left-bar.component.ts
+++ FrontEnd/src/components/left-bar/left-bar.component.ts
@@ -4,12 +4,14 @@
 
 import {Component} from 'angular2/core';
 import {MenuComponent} from "../menu/menu.component";
+import {GardenViewComponent} from "../garden-view/garden-view.component";
+import {GeneralInfoComponent} from "../general-info/general-info.component";
 
 @Component({
     selector: 'left-bar',
     moduleId: module.id,
     templateUrl: './left-bar.component.html',
     styleUrls : ['./left-bar.component.css'],
-    directives: [MenuComponent]
+    directives: [MenuComponent, GardenViewComponent, GeneralInfoComponent]
 })
 export class LeftBarComponent { }
