8e2bc9fd0e1581dca2e8f648803d2dd6eafbd9e1
NablaT <remi.pourtier@laposte.net>
2016-05-07 11:06:02 +0200

    Adding offer frame

--- FrontEnd/src/components/garden-list/garden-list.component.ts
+++ FrontEnd/src/components/garden-list/garden-list.component.ts
@@ -17,12 +17,16 @@ export class GardenListComponent {
     public gardenListPrivate:GardenComponent[];
 
     constructor(){
-        let garden=new GardenComponent("Nice", "Le marché de claudette",50, 20, 0, true,false, "", "", "", "public");
-        let garden2=new GardenComponent("Saint Raphael", "A le bon potager",60, 30, 0, true,false, "", "", "", "public");
+        let garden=new GardenComponent("Nice", "Claudette garden",50, 20, 0, true,false, "", "", "../../assets/imageLocation/start-vegetable-garden.jpg", "public");
+        let garden2=new GardenComponent("Saint Raphael", "A le bon potager",60, 30, 0, true,false, "", "", "../assets/imageLocation/image4.png", "public");
         this.gardenListPublic=[garden, garden2];
 
-        let garden3=new GardenComponent("Nice", "Le marché de claudette",50, 20, 0, true,false, "", "", "", "private");
-        let garden4=new GardenComponent("Saint Raphael", "A le bon potager",60, 30, 0, true,false, "", "", "", "private");
+        let garden3=new GardenComponent("Nice", "Le marché de claudette",50, 20, 0, true,false, "", "",  "../assets/imageLocation/image3.png", "private");
+        let garden4=new GardenComponent("Saint Raphael", "A le bon potager",60, 30, 0, true,false, "", "", "../assets/imageLocation/image2.png", "private");
         this.gardenListPrivate=[garden3, garden4];
+    }
+
+    loginPage(){
+        console.log("coucou");
     }
 }
