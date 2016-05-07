e7071713fdf7cb6c7b017731af43d4c2d69e0943
NablaT <remi.pourtier@laposte.net>
2016-05-07 12:19:07 +0200

    Fixing design for garden view

--- FrontEnd/src/components/garden-view/garden-view.component.ts
+++ FrontEnd/src/components/garden-view/garden-view.component.ts
@@ -10,4 +10,10 @@ import {Component} from 'angular2/core';
     templateUrl: './garden-view.component.html',
     styleUrls : ['./garden-view.component.css']
 })
-export class GardenViewComponent { }
+export class GardenViewComponent {
+
+
+    getInfo(){
+        console.log("coucou");
+    }
+}
